8ce80e931a2566dc456bb66074ee50b66d9fcc63
Anslie Brant <brantae@gmail.com>
2023-05-08 19:49:55 -0500

    commit tree faa555d4965ea0ebe78b29c8ab0f2928adee7500

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :is_clickbaity?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def is_clickbaity?
+        if CLICKBAIT.none? { |phrase| phrase.match title }
+            errors.add(:title, "Must include clickbait phrase")
+        end
+    end
 end
